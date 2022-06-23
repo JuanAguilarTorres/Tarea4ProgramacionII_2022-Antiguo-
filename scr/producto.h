daceb00103de9a98c4dab29193c08bdb4d6932aa
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2022-06-23 00:35:55 -0600

    Creados archivos para clases.

---
+++ scr/producto.h
@@ -0,0 +1,10 @@
+#ifndef PRODUCTO_H
+#define PRODUCTO_H
+
+using namespace std;
+
+class Producto {
+
+};
+
+#endif
---
+++ scr/tienda.h
@@ -0,0 +1,12 @@
+#ifndef TIENDA_H
+#define TIENDA_H
+
+#include "producto.h"
+
+using namespace std;
+
+class Tienda {
+
+};
+
+#endif
