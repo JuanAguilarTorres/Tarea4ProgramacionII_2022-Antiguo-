0f6cbb2c5d6b4f584989892ee977a564a852b75e
Juan Aguilar <Juan.aguilartorres@ucr.ac.cr>
2022-06-28 03:20:13 -0600

    Test de stream de salida en producto

---
+++ tests/producto_test.cpp
@@ -0,0 +1,32 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+
+#include "./../src/producto.h"
+
+using namespace std;
+
+namespace
+{
+    TEST(Producto_Test, Operador_de_Salida_Test)
+    {
+        /// AAA
+
+        // Arrange - configurar el escenario
+        Producto *producto1 = new Producto(1,"Detergente", 5);
+
+        // Act - ejecute la operación
+
+        std::ostringstream stream;
+        stream << producto1;
+
+        std::string actual = stream.str();
+        std::string esperado = "[1] - Detergente, Cantidad: 5";
+
+        delete producto1;
+        // Assert - valide los resultados
+        EXPECT_EQ(esperado, actual);
+    }
+}
+    
